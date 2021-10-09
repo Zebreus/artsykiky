c5db838c3173f8bdd50091fe84ecf6025419550d
Franzi <franzi.chaluppa@gmail.com>
2021-10-09 17:19:19 +0200

    added redirect for pages

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/html/3Dcube.html',
+        destination: '/3Dcube',
+        permanent: true,
+      },
+      {
+        source: '/html/labyrinth.html',
+        destination: '/labyrinth',
+        permanent: true,
+      },
+    ]
+  },
 }
